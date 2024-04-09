70e7f375c60028bfec729fca6b735b3bfde40781
khaireddinechebbi <khaireddine.chebbi@live.fr>
2024-04-09 11:39:41 +0100

    binary tree height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,19 +6,16 @@
 */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-size_t l, r;
+size_t l = 0, r = 0;
 
 if (tree == NULL)
 return (0);
 
-if (tree->left == NULL)
-l = 0;
-
-if (tree->right == NULL)
-r = 0;
-
+if (tree->left != NULL)
 l = binary_tree_height(tree->left) + 1;
+
+if (tree->right != NULL)
 r = binary_tree_height(tree->right) + 1;
 
-return (l > r ? l : r);
+return (l > r ? l : r)
 }
